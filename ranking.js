c22154c98f8262f4277f2039c1d47204cad2d89a
JoleG <lewinnekjorge@gmail.com>
2024-01-12 20:05:33 -0500

    Add files via upload

---
+++ ranking.js
@@ -0,0 +1,6 @@
+[{"alumno":"Jorge","nota":9.8,"ranking":1},{"alumno":"Katty","nota":9.1,"ranking":2},
+{"alumno":"Patricia","nota":8.09,"ranking":3},{"alumno":"Ivan","nota":7,"ranking":7},
+{"alumno":"Juan","nota":6.09,"ranking":10},{"alumno":"Maria","nota":5,"ranking":20},
+{"alumno":"Daniel","nota":4.09,"ranking":21},{"alumno":"Yolanis","nota":4,"ranking":22},
+{"alumno":"Roberto","nota":3.09,"ranking":23},{"alumno":"Javier","nota":2.5,"ranking":26},
+{"alumno":"Jesus","nota":2,"ranking":30},{"alumno":"Luz","nota":1,"ranking":50}]
